118f5a5e9d7cdff73c754e56c61935bbb9bcc2b4
brunopires01 <159458608+brunopires01@users.noreply.github.com>
2025-07-07 15:08:03 +0000

    ferias

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação'
+    'o que é python?',
+    'o python é uma linguagem de progamação'
+)
+
+criaCartao(
+    'geografia'
+    'qual a capitao da França?'
+    'a capitao da França é Paris'
+)
+
+criaCartao(
+    'programação'
+    'o que é função'
+    'uma função é um bloco de codigo que executa alguma tarefa'
+)
